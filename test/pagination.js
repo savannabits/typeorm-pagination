d099514452a47f7ed13c1e5f0d973aa9d79a4d98
Sam Arossi Maosa <smaosa@strathmore.edu>
2021-06-19 22:07:49 +0300

    Configured tests with chai and mocha

---
+++ test/pagination.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+describe("Pagination Tests", function() {
+    it("tests can run", function() {
+        let res = 1+1;
+        expect(res).to.equal(2);
+    })
+})
